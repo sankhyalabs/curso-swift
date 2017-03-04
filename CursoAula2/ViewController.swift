de50b8a1494ea219a0a8b6664d50f8a9f81ce89f
Gildo Vieira da Cunha Neto <gildoneto@Gildos-Mac.local>
2017-03-04 03:34:26 -0800

    Initial Commit

---
+++ CursoAula2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CursoAula2
+//
+//  Created by Gildo Vieira da Cunha Neto on 04/03/17.
+//  Copyright © 2017 Gildo Vieira da Cunha Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
